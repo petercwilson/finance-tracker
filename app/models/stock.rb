8c264a1fb2d2bb2700a0974fa8a19e6c077cee0b
Peter Wilson <pcwilson19@gmail.com>
2020-04-01 14:43:27 -0700

    handle js errors

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,10 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   secret_token: 'Tsk_9bc561644b344032ba921333413320ed',
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker_symbol)
+    begin 
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
 end
